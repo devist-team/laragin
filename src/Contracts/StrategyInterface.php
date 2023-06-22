de5eb6b30ef0448be38356bdd8e568eb4c556f23
Abbas mkhzomi <llabbasmkhll@gmail.com>
2023-06-22 20:03:18 +0330

    add strategy base class and interface

---
+++ src/Contracts/StrategyInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Devist\Laragin\Contracts;
+
+interface StrategyInterface
+{
+
+}
---
+++ src/Strategies/Strategy.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Devist\Laragin\Strategies;
+
+use Closure;
+use Devist\Laragin\Contracts\StrategyInterface;
+use Illuminate\Support\Facades\App;
+
+class Strategy implements StrategyInterface
+{
+
+}

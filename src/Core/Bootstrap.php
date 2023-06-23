3f319581a13fa4c02719b8cc1cdf125d6bd1dfff
Abbas mkhzomi <llabbasmkhll@gmail.com>
2023-06-23 22:14:05 +0330

    extract default routes to bootstrap

--- src/Core/Bootstrap.php
+++ src/Core/Bootstrap.php
@@ -3,6 +3,7 @@
 namespace Devist\Laragin\Core;
 
 use Closure;
+use Devist\Laragin\Controllers\Controller;
 use Devist\Laragin\Strategies\OTP;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,8 @@ class Bootstrap
     public static function routes(): Closure
     {
         return function () {
+            self::ِdefaultRoutes();
+
             Route::prefix('{guard}')->group(function () {
                 foreach (config('laragin.strategies') as $strategy) {
                     self::resolve($strategy)::routes();
@@ -23,8 +26,16 @@ class Bootstrap
         };
     }
 
-    private static function resolve(string $strategy)
+    private static function resolve(string $strategy): mixed
     {
         return self::$strategies[$strategy];
+    }
+
+    private static function ِdefaultRoutes(): void
+    {
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('me', [Controller::class, 'index']);
+            Route::get('logout', [Controller::class, 'delete']);
+        });
     }
 }
--- src/Strategies/OTP.php
+++ src/Strategies/OTP.php
@@ -10,7 +10,6 @@ class OTP extends Strategy
     static function routes(): void
     {
         Route::prefix('otp')->group(function () {
-            Route::middleware('auth:sanctum')->get('me', [OTPController::class, 'index']);
             Route::post('login', [OTPController::class, 'login']);
             Route::post('send', [OTPController::class, 'sendOtp']);
         });
